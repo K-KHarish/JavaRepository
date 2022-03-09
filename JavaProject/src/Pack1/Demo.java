a0c70b5aee197bb09ca18ec8dedb1a32ef32224d
k501477 <k501477@VW10A26927.principalusa.corp.principal.com>
2022-03-09 11:51:01 +0530

    Lambda expression has been added inside java project

--- JavaProject/src/Pack1/Demo.java
+++ JavaProject/src/Pack1/Demo.java
@@ -1,13 +1,12 @@
 package Pack1;
 
 import Pack1.MyInterface;
-import Pack1.SomeClass;
 
 public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MyInterface ref=SomeClass.getMyInterface();
+		MyInterface ref=()->{System.out.println("Inside lamda disp");};
 		ref.disp();
 	}
 
